e5cb4f06dd768ee8d8491b35b883e86064b6bfa4
Lisa Stillwell <lisa@renci.org>
2025-10-21 15:01:30 -0400

    #5 updated code to use less memory by chunking all processing

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### Example usage
 ```
-usage: main.py [-h] -i INPUT -c CONF [-q QDRANT_URL] [--csv] [--json]
+usage: python main.py [-h] -i INPUT -c CONF [-q QDRANT_URL] [--csv] [--json]
 
 frink-embeddings
 
@@ -11,5 +11,29 @@ options:
   -i INPUT, --input INPUT                  An hdt file from an rdf graph
   -c CONF, --conf CONF                     The yaml file for configuration
   -q QDRANT_URL, --qdrant_url QDRANT_URL   The url for the Qdrant client
+  -n COLLECTION_NAME, --collection_name COLLECTION_NAME
+                                           The name of the Qdrant collection (required if using Qdrant)
   --tsv                                    Write the output to a tsv file
   --json                                   Write the output to a json file formatted for Qdrant upload
+```
+
+### IMPORTANT NOTE for adding data to an existing Qdrant collection
+This application uses sequential integers for Qdrant data point ids.
+By default, the Qdrant client will start adding data to a collection
+with point id = 0.
+The code can be modified to use a different starting point id, if you
+are trying to add data to an existing collection with the same scheme of
+using sequential integers for point ids. 
+There is a global variable to keep track of point ids for the Qdrant DB
+inserts, named **idx**. This is set to 0 by default.
+The Qdrant data point ids need to be unique.
+If adding data to an existing Qdrant DB, the src/main.py file must be
+edited to set the **idx** variable to the highest point id in the existing
+Qdrant DB collection. It will be incremented by 1 when it is used. For instance,
+setting idx = 4895902, will start adding data with point id = 4895903.
+### FAILURE TO MANUALLY UPDATE THE IDX VARIABLE WILL OVERWRITE EXISTING DATA ###
+
+
+#### Another Note:
+The graph name, which is stored as metadata for the Qdrant collection
+points, is derived from the input hdt file name.
--- src/main.py
+++ src/main.py
@@ -11,6 +11,15 @@ from rdflib import URIRef
 from rdflib_hdt import HDTDocument
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient, models
+import torch
+
+# idx is a global to keep track of point ids for the Qdrant DB
+# inserts they need to be unique and not reset when performing
+# chunking of data
+# if adding data to an existing Qdrant collection,this number
+# must be set to the integer point id in the collection - it will
+# be incremented by 1 when it is used.
+idx = 0
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -21,6 +30,7 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout)
     ]
 )
+
 
 # create embeddings and write to a tsv file
 def saveToTSV(model, input_file, sentences_to_embed_dict):
@@ -45,18 +55,19 @@ def saveToTSV(model, input_file, sentences_to_embed_dict):
 # create embeddings and write to a json file that is in
 # a format suitable for uploading into a vector database
 def saveToJSON(model, input_file, sentences_to_embed_dict):
+    global idx
     graph_name = os.path.splitext(os.path.basename(input_file))[0]
     json_file = os.path.splitext(os.path.basename(input_file))[0] + ".json"
     dict_list = []
-    idx = 1
+
     try:
         # create an embedding for each unique subject
         # and write to the json file
         for k, v in sentences_to_embed_dict.items():
+            idx += 1
             embedding = model.encode(v)
             embedded_dict = {"id": idx, "vector": embedding.tolist(), "payload": {"graph": graph_name, "iri": k, "label": v}}
             dict_list.append(embedded_dict)
-            idx += 1
 
         with open(json_file, 'w') as f:
             json.dump({'points': dict_list}, f, indent=3)
@@ -68,42 +79,41 @@ def saveToJSON(model, input_file, sentences_to_embed_dict):
 
 # create an embedding for each unique subject
 # and write to a qdrant collection
-def saveToQdrant(model, url, input_file, sentences_to_embed_dict):
+def saveToQdrant(model, url, collection_name, input_file, sentences_to_embed_dict):
     # connect to Qdrant client
+    global idx
     try:
-        client = QdrantClient(url=url, timeout=30)
-        # create a collection name
-        collection_name = os.path.splitext(os.path.basename(input_file))[0]
-        if client.collection_exists(collection_name):
-            # collection already exists so notify and exit
-            logger.info(f"Collection '{collection_name}' already exists.")
-            exit(0)
-        else:
-            # collection does not exist - continue to create and populate
-            logger.debug(f"Collection '{collection_name}' does not exist.")
+        client = QdrantClient(url=url, timeout=60)
+        # create graph name to add to point payload
+        # use .hdt input file name
+        graph_name = os.path.splitext(os.path.basename(input_file))[0]
+
+        if not client.collection_exists(collection_name):
+            # collection does not exist - create and populate
+            logger.debug(f"Collection '{collection_name}' does not exist - creating.")
             client.create_collection(collection_name=collection_name,
                                      vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE))
-            idx = 0
-            points = []
+        points = []
 
-            for k, v in sentences_to_embed_dict.items():
-                idx += 1
-                embedding = model.encode(v)
-                points.append(
-                    models.PointStruct(
-                        id=idx,  # Unique ID for each point
-                        vector=embedding,
-                        payload = {"graph": collection_name, "iri": k, "label": v}  # Add metadata (payload)
-                    )
+        for k, v in sentences_to_embed_dict.items():
+            idx += 1
+            embedding = model.encode(v)
+            points.append(
+                models.PointStruct(
+                    id=idx,  # Unique ID for each point
+                    vector=embedding,
+                    payload={"graph": graph_name, "iri": k, "label": v}  # Add metadata (payload)
                 )
-                # batch upserts to avoid timeouts
-                if idx % 1000 == 0:
-                    client.upsert(collection_name=collection_name, points=points)
-                    points = []
+            )
+            # batch upserts to avoid timeouts
+            if idx % 1000 == 0:
+                client.upsert(collection_name=collection_name, points=points)
+                points = []
 
-            # final upsert
-            client.upsert(collection_name=collection_name, points=points)
-            logger.info(f"Saved embeddings to Qdrant collection '{collection_name}'.")
+        # final upsert
+        client.upsert(collection_name=collection_name, points=points)
+        logger.info(f"Saved embeddings to Qdrant collection '{collection_name}'.")
+        logger.info(f"last point id was {points[-1].id}")
 
     except Exception as e:
         logger.error(f"An error occurred uploading data to Qdrant:{url}: {e}")
@@ -205,51 +215,84 @@ def createSentences(embed_list):
 
 
 # create embeddings from rdf triples
-def main(input_file: pathlib.Path, config_file: pathlib.Path, tsv_output, json_output, qdrant_url):
+def main(input_file: pathlib.Path, config_file: pathlib.Path, tsv_output, json_output, qdrant_url, collection_name):
     logger.info(f"input: {input_file}  config: {config_file}")
 
-    doc = HDTDocument(str(input_file))
+    doc = HDTDocument(str(input_file), indexed=False)
     logger.info(f"subjects: {doc.nb_subjects}  predicates: {doc.nb_predicates}  objects: {doc.nb_objects}")
 
     iri_types = getIriTypes(config_file)
     iri_values = getAllIriTypeValues(iri_types)
-
     sentences_to_embed_dict = {}
     embed_list = []
     model = None
-    triples, cardinality = doc.search((None, None, None))
 
-    try:
-        # first collect all of the triples desired - as defined
-        # by the predicates listed in the config file
-        for s, p, o in triples:
-            if str(p) in iri_values and isinstance(p, URIRef):
-                key = getIriKeyFromValue(iri_types, str(p))
-                embed_dict = {"config_key": key, "subject": str(s), "predicate": str(p), "object": str(o)}
-                embed_list.append(embed_dict)
-
-        # now create a list of sentences to embed
-        sentences_to_embed_dict = createSentences(embed_list)
-
-
-    except Exception as e:
-        logger.error(f"An error occurred while parsing file {input_file}: {e}")
     try:
         # load embedding model
         model = SentenceTransformer('all-MiniLM-L6-v2')
         logger.debug("loaded model")
+        # Check if CUDA is available
+        # print("CUDA available:", torch.cuda.is_available())
+        # Check if model parameters are on GPU
+        # print("Model is on GPU:", next(model.parameters()).is_cuda)
     except Exception as e:
         logger.error(f"An error occurred while loading the embedding model: {e}")
 
-        # now see how we want to embed and save this data
-    if tsv_output:
-        saveToTSV(model, input_file, sentences_to_embed_dict)
+    # in order to use the least amount of memory, we will process the
+    # triples grouped by subject - thus avoiding loading all the triples
+    # into memory at once
+    # first store all the unique subjects
+    all_subjects = set()
+    triples, cardinality = doc.search((None, None, None))
+    for s, p, o in triples:
+        all_subjects.add(s)
+    # convert the set to a list for chunking
+    subject_list = list(all_subjects)
 
-    if json_output:
-        saveToJSON(model, input_file, sentences_to_embed_dict)
+    # iterate through the subject list in chunks
+    subject_chunk_size = 1000
+    for i in range(0, len(subject_list), subject_chunk_size):
+        try:
+            subject_chunk = subject_list[i:i + subject_chunk_size]
 
-    if qdrant_url is not None:
-        saveToQdrant(model, qdrant_url, input_file, sentences_to_embed_dict)
+            # reset the sentences to embed dict and embed_list for each chunk
+            sentences_to_embed_dict = {}
+            embed_list = []
+            # Process each subject in the current chunk
+            for subject in subject_chunk:
+                # Get all triples for the current subject
+                triples, cardinality = doc.search((subject, None, None))
+
+                # collect all the triples desired - as defined
+                # by the predicates listed in the config file
+                for s, p, o in triples:
+                    if str(p) in iri_values and isinstance(p, URIRef):
+                        # print(f"Found a match: {s}  {p}  {o}")
+                        key = getIriKeyFromValue(iri_types, str(p))
+                        embed_dict = {"config_key": key, "subject": str(s), "predicate": str(p), "object": str(o)}
+                        embed_list.append(embed_dict)
+        except Exception as e:
+            logger.error(f"An error occurred while parsing HDT file {input_file}: {e}")
+
+        try:
+            # now create a list of sentences to embed
+            sentences_to_embed_dict = createSentences(embed_list)
+        except Exception as e:
+                logger.error(f"An error occurred creating embedding sentences: {e}")
+
+        # now see if we have data, and how we want to embed and save this data
+        try:
+            if sentences_to_embed_dict is not None:
+                if tsv_output:
+                    saveToTSV(model, input_file, sentences_to_embed_dict)
+
+                if json_output:
+                    saveToJSON(model, input_file, sentences_to_embed_dict)
+
+                if qdrant_url is not None:
+                    saveToQdrant(model, qdrant_url, collection_name, input_file, sentences_to_embed_dict)
+        except Exception as e:
+            logger.error(f"An error occurred saving the embedded sentences: {e}")
 
 
 if __name__ == '__main__':
@@ -258,10 +301,13 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', required=True, type=pathlib.Path, help='An hdt file from an rdf graph')
     parser.add_argument('-c', '--conf', required=True, type=pathlib.Path, help='The yaml file for configuration')
     parser.add_argument('-q', '--qdrant_url', required=False, help='The url for the Qdrant client')
+    parser.add_argument('-n', '--collection_name', required=False, help='The name of the Qdrant collection')
     parser.add_argument('--tsv', action='store_const', const=True, help='Write the output to a tsv file')
     parser.add_argument('--json', action='store_const', const=True, help='Write the output to a json file')
     args = parser.parse_args()
     if not (args.tsv or args.json or args.qdrant_url):
         parser.error("At least one of --tsv, --json or --qdrant_url is required.")
+    if args.qdrant_url and args.collection_name is None:
+        parser.error("--collection_name is required when using --qdrant_url.")
 
-    main(args.input, args.conf, args.tsv, args.json, args.qdrant_url)
+    main(args.input, args.conf, args.tsv, args.json, args.qdrant_url, args.collection_name)
